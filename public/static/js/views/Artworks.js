d8aabd21ce9c38a63c6937d0d1bb82df7a849d7d
nevge336 <neveu.ge@gmail.com>
2023-12-07 14:27:51 -0500

    afficher album bootstrap

--- public/static/js/views/Artworks.js
+++ public/static/js/views/Artworks.js
@@ -23,7 +23,7 @@ export default class extends AbstractView {
 
         // Get the data from the specific topic JSON file
         const result = await getData(`/static/data/${this.topic}.json`)
-        
+
         // Get the image for each artwork
         const iiifUrl = "https://www.artic.edu/iiif/2/";
 
@@ -36,22 +36,31 @@ export default class extends AbstractView {
             const artworkData = artworksData[i];
             const imageUrl = `${iiifUrl}/${artworkData.data['image_id']}/full/200,/0/default.jpg`;
             listArtworks += `
-                                <div class="col-md-3">
-                                    <a href="/artwork-view/${result.data[i]['id']}?topic=${this.topic}" data-link>
-                                        ${result.data[i]['title']}
-                                    </a>
-                                    <img src="${imageUrl}" class="img-fluid">
+                                <div class="col">
+                                    <div class="card shadow-sm">
+                                        <img src="${imageUrl}" class="bd-placeholder-img object-fit-cover card-img-top" width="100%" height="225">
+                                            <div class="card-body">
+                                                <p class="card-text">${result.data[i]['title']}</p>
+                                                <div class="d-flex justify-content-between align-items-center">
+                                                    <div class="btn-group">
+                                                        <a href="/artwork-view/${result.data[i]['id']}?topic=${this.topic}" data-link class="btn btn-sm btn-outline-secondary">View</a>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                    </div>
                                 </div>
                             `;
         }
 
         return `
-            <section class="container text-center gy-5">
-            <h1>Topic: ${this.topic}</h1>
-                <div class="row">
-                    ${listArtworks}
+            <div class="album py-5 bg-body-tertiary">
+                <div class="container">
+                <h1>Topic: ${this.topic}</h1>
+                    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                            ${listArtworks}
+                    </div>
                 </div>
-            </section>
+            </div>
         `;
     }
 }
